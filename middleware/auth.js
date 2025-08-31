fb05e39b6e94a044f2bbbe6ecef59bca826f6f56
AYUSH-2858 <thakurayush24681@gmail.com>
2025-08-31 14:51:06 +0530

    Error

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const auth = (req, res, next) => {
     const token = req.cookies.token;
     if (!token) {
-        return res.status(401).json({ message: "No token, authorization denied" });
+        return res.status(402).json({ message: "No token, authorization denied" });
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
